5bfc6bac48e6021ccc4da2a24b5a68ad0b64f72b
Soumya Satish <88482100+soumya-embold@users.noreply.github.com>
2021-11-22 17:22:35 +0530

    Add files via upload

--- Entity.h
+++ Entity.h
@@ -2,6 +2,12 @@
 
 class Entity
 {
+private:
+	int m_array[10];
 public:
 	void setSize(int num, int size);
+	void fun(int p, int q);
+	void fun_ref(int& a, int& b);
+	void foo(int x, int y);
+	void bar(int m, int n);
 };
--- Main.cpp
+++ Main.cpp
@@ -3,11 +3,24 @@
 
 int main()
 {
-	int n, value;
-	std::cin >> n;
+	int number, value;
+	std::cin >> number;
 	std::cin >> value;
 
 	Entity e;
-	e.setSize(n, value);
+	e.setSize(number, value);
+
+	if (number >= 0 && number < 10)
+		e.fun(number, value);			// Compliant
+
+	if (value >= 0 && value < 10)
+		e.fun(number, value);			// Non-compliant
+
+	e.fun_ref(number, value);
+	
+	e.foo(number, value);
+	
+	e.bar(number, value);
+
 	return 0;
 }
